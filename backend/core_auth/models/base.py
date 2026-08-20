c3f21d75238956683ed8ecea87cfc8b7b1768c18
Moein Omidi <moeinomidii@gmail.com>
2026-08-20 14:28:13 +0330

    fix the way we create superusers in terminal + dependency updates
    Signed-off-by: Moein Omidi <moeinomidii@gmail.com>

--- backend/core_auth/models/base.py
+++ backend/core_auth/models/base.py
@@ -2,17 +2,41 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+from django.contrib.auth.models import BaseUserManager
+
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email must be set')
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_unusable_password()
+
+        # Check if a password was provided; if not, set it as unusable
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        # Validation to ensure the superuser flags are set correctly
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
