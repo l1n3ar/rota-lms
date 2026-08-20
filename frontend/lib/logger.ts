1500f2c81585cffcd0202a7e319ffa018b2de769
l1n3ar <66662596+l1n3ar@users.noreply.github.com>
2026-08-20 17:59:07 +0530

    use pino to later output to datadog or whatever

---
+++ frontend/lib/logger.ts
@@ -0,0 +1,8 @@
+import pino from "pino"
+
+const isDev = process.env.NODE_ENV !== "production"
+
+export const logger = pino({
+    level: isDev ? "debug" : "info",
+    transport: isDev ? { target: "pino-pretty" } : undefined,
+})
