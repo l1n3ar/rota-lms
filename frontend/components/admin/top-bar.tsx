6afa8a5c75cc1287244e3ff7ac22d4b8ef2b9d65
l1n3ar <66662596+l1n3ar@users.noreply.github.com>
2026-08-21 15:32:08 +0530

    Admin top bar complete

--- frontend/components/admin/top-bar.tsx
+++ frontend/components/admin/top-bar.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Button } from '../ui/button'
-import { TextAlignStart } from 'lucide-react'
+import { Bell, Search, Settings, TextAlignStart } from 'lucide-react'
+
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupInput,
+} from "@/components/ui/input-group"
 
 const AdminTopBar = () => {
     return (
         <div className='w-full flex items-center justify-between'>
 
             <div className='flex items-center gap-4'>
-                <Button className='rounded-full p-4x shrink-0' size='icon-lg' variant='outline'><TextAlignStart /></Button>
+                <Button className='rounded-full p-4x shrink-0' size='icon-xl' variant='outline'><TextAlignStart /></Button>
                 <div className='flex flex-col'>
                     <span className='text-2xl'>Hello Hossein</span>
                     <span className='text-xs text-neutral-500'>Let's learn something new today!</span>
@@ -16,6 +22,21 @@ const AdminTopBar = () => {
             </div>
 
             <div className='flex items-center gap-2'>
+                <InputGroup className='bg-white p-6 flex w-[20rem]'>
+
+                    <InputGroupInput
+                        id="inline-end-input"
+                        placeholder="Search from courses..."
+                    />
+
+                    <InputGroupAddon align="inline-end">
+                        <Search />
+                    </InputGroupAddon>
+
+                </InputGroup>
+
+                <Button className='rounded-full p-4x shrink-0' size='icon-lg' variant='outline'><Bell /></Button>
+                <Button className='rounded-full p-4x shrink-0' size='icon-lg' variant='outline'><Settings /></Button>
 
             </div>
 
