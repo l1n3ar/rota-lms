3697490270a1d0e8c6f31cea6c4ce2f2d313f53d
l1n3ar <66662596+l1n3ar@users.noreply.github.com>
2026-08-21 15:34:26 +0530

    layout the children

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -1,10 +1,13 @@
 import AdminTopBar from '@/components/admin/top-bar'
-import React from 'react'
 
-const AdminLayout = () => {
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-full w-full p-8x bg-neutral-100'>
-        <AdminTopBar />
+    <div className='h-full w-full p-8x bg-neutral-100 flex flex-col gap-4'>
+      <AdminTopBar />
+      <div className='bg-white rounded-xl h-full w-full'>
+        {children}
+      </div>
+
     </div>
   )
 }
