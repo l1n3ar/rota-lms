0dea384bb1719a875ef3c9609e49cc1aac823977
l1n3ar <66662596+l1n3ar@users.noreply.github.com>
2026-08-22 12:06:47 +0530

    add tooltip

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,8 +3,9 @@ import { Geist_Mono, Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -24,8 +25,10 @@ export default function RootLayout({
     >
       <body>
         <ThemeProvider>
-          {children}
-          </ThemeProvider>
+          <TooltipProvider>
+            {children}
+          </TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
