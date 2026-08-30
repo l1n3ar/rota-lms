7dc3335303749c46ca609243c5dc80c632595939
l1n3ar <66662596+l1n3ar@users.noreply.github.com>
2026-08-30 16:47:01 +0530

    add all missing endpoints

--- frontend/lib/api/endpoints/index.ts
+++ frontend/lib/api/endpoints/index.ts
@@ -1,7 +1,13 @@
 import { AUTH_ENDPOINTS } from "./auth";
 import { TICKET_ENDPOINTS } from "./tickets";
+import { COURSE_ENDPOINTS } from "./course";
+import { QUIZ_ENDPOINTS } from "./quiz";
+import { CERTIFICATE_ENDPOINTS } from "./certificate";
 
 export const ENDPOINTS  = {
     AUTH : {...AUTH_ENDPOINTS},
-    TICKET : {...TICKET_ENDPOINTS}
+    TICKET : {...TICKET_ENDPOINTS},
+    COURSE : {...COURSE_ENDPOINTS},
+    QUIZ : {...QUIZ_ENDPOINTS},
+    CERTIFICATE : {...CERTIFICATE_ENDPOINTS}
 }
